dba7836dfbf6984f15e36fe90b02026bd5435041
Martin Bäumer <martin.baeumer@gmail.com>
2023-05-14 17:47:14 +0200

    added base version of a Java implementation

---
+++ java-base/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -0,0 +1,12 @@
+package com.gildedrose;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class GildedRoseTest {
+
+    @Test
+    void updateQuality() {
+    }
+}
