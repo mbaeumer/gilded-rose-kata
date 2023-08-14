a12a795ae1b162b98c425119f30900489f62a436
Martin Bäumer <martin.baeumer@gmail.com>
2023-08-14 22:54:10 +0200

    added test for the Java version

--- java-base/src/test/java/com/gildedrose/GildedRoseTest.java
+++ java-base/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -8,5 +8,14 @@ class GildedRoseTest {
 
     @Test
     void updateQuality() {
+        Item[] items = new Item[5];
+        Item brie = new Item("Aged Brie", 15, 50);
+        items[0] = brie;
+        GildedRose gildedRose = new GildedRose(items);
+        gildedRose.updateQuality();
+        Item actualItem = items[0];
+        assertTrue("Aged brie".equals(actualItem.name));
+        assertEquals(actualItem.sellIn, 13);
+        assertEquals(actualItem.quality, 49);
     }
 }
