ce1a31eca90f4a7ae7997aa28a634dac635c22d0
Martin Bäumer <martin.baeumer@gmail.com>
2023-08-20 02:06:36 +0200

    fixed compile error

--- csharp/GildedRose/Program.cs
+++ csharp/GildedRose/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace GildedRoseKata
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            IList<Item> Items = new List<Item>{
+                new Item {Name = "+5 Dexterity Vest", SellIn = 10, Quality = 20},
+                new Item {Name = "Aged Brie", SellIn = 2, Quality = 0},
+                new Item {Name = "Elixir of the Mongoose", SellIn = 5, Quality = 7},
+                new Item {Name = "Sulfuras, Hand of Ragnaros", SellIn = 0, Quality = 80},
+                new Item {Name = "Sulfuras, Hand of Ragnaros", SellIn = -1, Quality = 80}};
+            GildedRose gildedRose = new GildedRose(Items);
+            gildedRose.UpdateQuality();
+        }
+    }
+}
